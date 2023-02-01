adacdda5da4116b95c3c06722023a7da139432ef
Nikola Horvat <the.final.frontend@gmail.com>
2023-02-01 11:51:39 +0100

    chore: add mongodb connection and tests

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,12 @@
 import 'module-alias/register'
+import dbConnection from '@infrastructure/db/mongodb/helpers/db-connection'
+import setupApp from '@main/config/app'
+import env from './config/env'
+
+console.log('here')
+
+dbConnection.connect(env.mongoUrl).then(() => {
+  console.log('MongoDB connected')
+  const app = setupApp()
+  app.listen(env.port, () => { console.log(`Server running on port ${env.port}`) })
+}).catch(console.error)
